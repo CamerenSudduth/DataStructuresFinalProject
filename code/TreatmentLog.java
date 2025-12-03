fcecd0ca59abcaf76d275fc04605dd8f6e03eca5
CamerenSudduth <therealcameren@gmail.com>
2025-12-03 11:29:55 -0600

    Update TreatmentLog.java

--- code/TreatmentLog.java
+++ code/TreatmentLog.java
@@ -2,64 +2,29 @@ package edu.hcu.triage;
 
 import java.util.*;
 
-/** Thin wrapper around PriorityQueue to enforce triage policy and utilities. */
-public class TriageQueue {
-    private final PriorityQueue<Patient> pq = new PriorityQueue<>(new TriageOrder());
+/** Append-only treatment log using LinkedList. */
+public class TreatmentLog {
+    private final LinkedList<TreatedCase> log = new LinkedList<>();
 
-    // TODO: enqueue(Patient p)
-    public void enqueue(Patient p){
-        if (p == null) { // Checks if patient is null
-            throw new IllegalArgumentException("Patient cannot be null");
-        }
-        pq.add(p); // Enqueue patient
-    }
-    // TODO: enqueueById(PatientRegistry reg, String id) - lookup then enqueue
-    public boolean enqueueById(PatientRegistry reg, String id) {
-        //makes sure input is not null
-        if (id == null) {
-            throw new IllegalArgumentException("ID cannot be null");
-        }
-        if (reg == null) {
-            throw new IllegalArgumentException("Patient Registry cannot be null");
-        }
-        Optional<Patient> p = reg.get(id);
-        if (p.isPresent()) { // Checks if patient is in registry
-            enqueue(p.get()); // Enqueue patient
-        }
-        return false;
-    }
-
-    // TODO: peekNext(): Optional<Patient>
-    public Optional<Patient> peekNext() {
-        return Optional.ofNullable(pq.peek()); // Peeks at next patient
-    }
-
-    // TODO: dequeueNext(): Optional<Patient>
-    public Optional<Patient> dequeueNext() {
-        return Optional.ofNullable(pq.poll()); // Retrieves and removes next patient
+    // TODO: append(TreatedCase tc)
+    public void append(TreatedCase tc) {
+        log.add(tc); //appends treated case to the log linked list
     }
 
     // TODO: size()
     public int size() {
-        return pq.size(); // Returns size of priority queue
+        return log.size(); //returns the size of the log linked list
     }
 
-    // TODO: snapshotOrder(): List<Patient> in triage order without mutating the queue
-    //       Hint: clone PQ or copy to an array then sort with same comparator.
-    public List<Patient> snapshotOrder() {
-        PriorityQueue<Patient> clone = new PriorityQueue<>(pq); // Clone priority queue
-        // takes clone structure and mutates it into a list of patient objects
-        List<Patient> patientList = new ArrayList<>();
-
-        while (!clone.isEmpty()) {
-            patientList.add(clone.poll()); // Remove from clone to maintain priority order
-        }
-        return patientList; //returns list following triage order
+    // TODO: asListOldestFirst() -
+    public LinkedList<TreatedCase> asListOldestFirst() {
+        return new LinkedList<>(log); // orders the linked list from oldest to newest
     }
 
-    // TODO: clear()
-    public void clear() {
-        pq.clear(); // Clears priority queue
+    // TODO: asListNewestFirst() -
+    public LinkedList<TreatedCase> asListNewestFirst() {
+        LinkedList<TreatedCase> clone = new LinkedList<>(log);
+        Collections.reverse(clone); //creates clone to not change normal order of log list
+        return clone; //returns reversed list of normal order that orders list from newest to oldest
     }
-
 }
